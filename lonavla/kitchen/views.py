d4b44999fa1925499ca51901bf18abc5b98ca1c0
Fathima28kousar <kousar28fathima@gmail.com>
2023-10-12 23:32:12 +0530

    S

--- lonavla/kitchen/views.py
+++ lonavla/kitchen/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Dish
 
 def dish(request):
-    return render(request,'dish.html')
+    if request.method == "POST":
+        data = request.POST
+        dish_name = data.get('dish_name')
+        dish_description = data.get('dish_description')
+        dish_image = request.FILES.get('dish_image')
+
+        if dish_name:  # Check if dish_name is not empty
+            Dish.objects.create(
+                dish_name=dish_name,
+                dish_description=dish_description,
+                dish_image=dish_image
+            )
+            return redirect('/dish/')
+
+    queryset = Dish.objects.all()
+    context = {'dish': queryset}
+
+    return render(request, 'dish.html', context)
